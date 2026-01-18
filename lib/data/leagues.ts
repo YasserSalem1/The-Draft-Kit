9fb98504153823b968bf305cab95a7f6c89497d3
Moghazy <mohamed_moghazy@etax.com.eg>
2026-01-18 21:19:22 +0200

    Fixed tournament fetching bug

--- lib/data/leagues.ts
+++ lib/data/leagues.ts
@@ -9,11 +9,11 @@ export interface League {
 }
 
 export const LEAGUES: League[] = [
-  { id: 'lck', name: 'LCK', regionName: 'LCK', logoUrl: '/tournamentLogos/lck_logo.png', parentId: '826679' },
-  { id: 'lpl', name: 'LPL', regionName: 'LPL', logoUrl: '/tournamentLogos/lpl_logo.png', parentId: '826789' },
-  { id: 'lec', name: 'LEC', regionName: 'LEC', logoUrl: '/tournamentLogos/lec_logo.png', parentId: '826906' },
-  { id: 'lcs', name: 'LCS', regionName: 'LCS', logoUrl: '/tournamentLogos/lcs_logo.png', parentId: '826763' },
-  { id: 'lta-north', name: 'LTA North', regionName: 'LTA North', logoUrl: '/tournamentLogos/lta_north_logo.png', parentId: '826763' },
-  { id: 'lta-south', name: 'LTA South', regionName: 'LTA South', logoUrl: '/tournamentLogos/lta_south_logo.png', parentId: '826775' },
+  { id: 'lck', name: 'LCK', regionName: 'LCK', logoUrl: '/tournamentLogos/lck_logo.png', parentId: '757825' },
+  { id: 'lpl', name: 'LPL', regionName: 'LPL', logoUrl: '/tournamentLogos/lpl_logo.png', parentId: '757828' },
+  { id: 'lec', name: 'LEC', regionName: 'LEC', logoUrl: '/tournamentLogos/lec_logo.png', parentId: '757782' },
+  { id: 'lcs', name: 'LCS', regionName: 'LCS', logoUrl: '/tournamentLogos/lcs_logo.png', parentId: '757843' },
+  { id: 'lta-north', name: 'LTA North', regionName: 'LTA North', logoUrl: '/tournamentLogos/lta_north_logo.png', parentId: '775630' },
+  { id: 'lta-south', name: 'LTA South', regionName: 'LTA South', logoUrl: '/tournamentLogos/lta_south_logo.png', parentId: '775635' },
 ];
 
